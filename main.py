c468383e1c136dbf2bcbf1ac6c5ef833256f0869
Teddy Christian <christianteddy90@gmail.com>
2026-02-09 14:09:41 +0100

    (kivy-app) initialize main kivy application with MainWidget

---
+++ main.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+from track import TrackWidget
+from sound_kit_service import SoundKitService
+
+Builder.load_file("track.kv")
+
+class MainWidget(RelativeLayout):
+    tracks_layout = ObjectProperty()
+    
+    def __init__(self, **kwargs):
+        super(MainWidget, self).__init__(**kwargs)
+        self.sound_kit_service = SoundKitService()
+
+    def on_parent(self, widget, parent):
+        for i in range(self.sound_kit_service.get_nb_tracks()):
+            sound = self.sound_kit_service.get_sound_at(i)
+            self.tracks_layout.add_widget(TrackWidget(sound=sound, sound_kit_service=self.sound_kit_service))
+
+class MrBeatApp(App):
+    pass
+
+MrBeatApp().run()
