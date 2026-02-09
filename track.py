1227934f25625ab0f7efa12c6d147eabea3aa0f8
Teddy Christian <christianteddy90@gmail.com>
2026-02-09 17:41:11 +0100

    (track-widget) connect button press to audio engine

--- track.py
+++ track.py
@@ -15,10 +15,16 @@ class TrackSoundButton(Button):
 class TrackWidget(BoxLayout):
     sound_kit_service = ObjectProperty()
     
-    def __init__(self, sound, **kwargs):
+    def __init__(self, sound, audio_engine, **kwargs):
         super(TrackWidget, self).__init__(**kwargs)
         sound_button = TrackSoundButton()
         sound_button.text = sound.displayname
+        sound_button.on_press = self.on_sound_button_press
         self.add_widget(sound_button)
+        self.audio_engine = audio_engine
+        self.sound = sound
         for i in range(0, TRACK_NB_STEPS):
             self.add_widget(TrackStepButton())
+
+    def on_sound_button_press(self):
+        self.audio_engine.play_sound(self.sound.samples)
