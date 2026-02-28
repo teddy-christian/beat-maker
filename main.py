4c08a6d7a86f68ef97c1e01c94c82ffb8c1ff895
Teddy Christian <christianteddy90@gmail.com>
2026-02-28 18:22:03 +0100

    (app-logic) wire AudioSourceMixer into MainWidget, TrackWidget step buttons, and SoundKit

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from audio_engine import AudioEngine
 
 Builder.load_file("track.kv")
 
+TRACK_NB_STEPS = 16
+
 class MainWidget(RelativeLayout):
     tracks_layout = ObjectProperty()
     
@@ -21,10 +23,14 @@ class MainWidget(RelativeLayout):
         self.audio_engine = AudioEngine()
         #self.audio_engine.play_sound(kick_sound.samples)
 
+        # self.audio_engine.create_track(kick_sound.samples, 120)
+
+        self.mixer = self.audio_engine.create_mixer(self.sound_kit_service.soundKit.get_all_samples(), 120, TRACK_NB_STEPS)
+
     def on_parent(self, widget, parent):
         for i in range(self.sound_kit_service.get_nb_tracks()):
             sound = self.sound_kit_service.get_sound_at(i)
-            self.tracks_layout.add_widget(TrackWidget(sound, self.audio_engine))
+            self.tracks_layout.add_widget(TrackWidget(sound, self.audio_engine, TRACK_NB_STEPS, self.mixer.tracks[i]))
 
 class MrBeatApp(App):
     pass
--- sound_kit_service.py
+++ sound_kit_service.py
@@ -23,6 +23,12 @@ class SoundKit:
     def get_nb_tracks(self):    
         return len(self.sounds)
 
+    def get_all_samples(self):
+        all_samples = []
+        for i in range(0, len(self.sounds)):
+            all_samples.append(self.sounds[i].samples)
+        return all_samples
+
 class SoundKit1(SoundKit):
     sounds=(Sound("sounds/kit1/kick1.wav", "KICK1"),
             Sound("sounds/kit1/clap1.wav", "CLAP1"),
