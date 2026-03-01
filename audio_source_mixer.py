b3c664df4b7c3ace82416f97b111e30fb2e6b357
Teddy Christian <christianteddy90@gmail.com>
2026-03-01 11:29:48 +0100

    (audio-source-mixer) add BPM control, play/stop toggle, clamped mixing with sum_16bits, and None guard

--- audio_source_mixer.py
+++ audio_source_mixer.py
@@ -2,22 +2,39 @@ from audiostream.sources.thread import ThreadSource
 from audio_source_track import AudioSourceTrack
 from array import array
 
+MAX_16BITS = 32767
+MIN_16BITS = -32768
+
+def sum_16bits(n):
+    s = sum(n)
+    if s > MAX_16BITS:
+        s = MAX_16BITS
+    if s < MIN_16BITS:
+        s = MIN_16BITS
+    return s
+
 class AudioSourceMixer(ThreadSource):
     buf = None
 
-    def __init__(self, output_stream, all_wav_samples, bpm, sample_rate, nb_steps, *args, **kwargs):
+    def __init__(self, output_stream, all_wav_samples, bpm, sample_rate, nb_steps, on_current_step_changed, min_bpm, *args, **kwargs):
         ThreadSource.__init__(self, output_stream, *args, **kwargs)
 
         self.tracks = []
         for i in range(0, len(all_wav_samples)):
-            track = AudioSourceTrack(None, all_wav_samples[i], bpm, sample_rate)
+            track = AudioSourceTrack(None, all_wav_samples[i], bpm, sample_rate, min_bpm)
             track.set_steps((0,) * nb_steps)
             self.tracks.append(track)
 
+        self.buf = None
+        self.silence = array('h', b"\x00\x00" * self.tracks[0].buffer_nb_samples)
+        self.bpm = bpm
         self.nb_steps = nb_steps
+        self.min_bpm = min_bpm
         self.current_sample_index = 0
         self.current_step_index = 0
         self.sample_rate = sample_rate
+        self.on_current_step_changed = on_current_step_changed
+        self.is_playing = False
 
     def set_steps(self, index, steps):
         if index >= len(self.tracks):
@@ -27,30 +44,44 @@ class AudioSourceMixer(ThreadSource):
             self.tracks[index].set_steps(steps)
 
     def set_bpm(self, bpm):
-        for i in range(0, len(self.tracks)):
-            self.tracks[i].set_bpm(bpm)
+        if bpm < self.min_bpm:
+            return
+        self.bpm = bpm
+
+    def audio_play(self):
+        self.is_playing = True
+
+    def audio_stop(self):
+        self.is_playing = False
 
     def get_bytes(self, *args, **kwargs):
 
+        for i in range(0, len(self.tracks)):
+            self.tracks[i].set_bpm(self.bpm)
+
         step_nb_samples = self.tracks[0].step_nb_samples
-        if self.buf is None or len(self.buf) != step_nb_samples:
-            self.buf = array('h', b"\x00\x00" * step_nb_samples)
+
+        if not self.is_playing:
+            return self.silence[0:step_nb_samples].tobytes()
 
         track_buffers = []
         for i in range(0, len(self.tracks)):
             track = self.tracks[i]
             track_buffer = track.get_bytes_array()
-            track_buffers.append(track_buffer)
+            if track_buffer is not None:  # guard against broken track
+                track_buffers.append(track_buffer)
 
-        for i in range(0, step_nb_samples):
-            # accumulate in int32 to avoid int16 overflow, then clamp
-            total = 0
-            for j in range(0, len(track_buffers)):
-                total += track_buffers[j][i]
-            self.buf[i] = max(-32768, min(32767, total))
+        s = map(sum_16bits, zip(*track_buffers))
+        self.buf = array('h', s)
+
+        if self.on_current_step_changed is not None:
+            step_index_for_display = self.current_step_index - 2 # The number of Sync
+            if step_index_for_display < 0:
+                step_index_for_display += self.nb_steps
+            self.on_current_step_changed(step_index_for_display)
 
         self.current_step_index += 1
         if self.current_step_index >= self.nb_steps:
             self.current_step_index = 0
 
-        return self.buf.tobytes()
+        return self.buf[0:step_nb_samples].tobytes()
